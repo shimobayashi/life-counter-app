eb41f4b793b5563c01f4655b427d3f86330f6917
shimobayashi <akimasashimobayashi@gmail.com>
2024-01-07 18:04:47 +0900

    ライフの上下をタップすることで増減できるようにした。

--- app/ui/life-counter.tsx
+++ app/ui/life-counter.tsx
@@ -1,49 +1,49 @@
 'use client';
 
 import React, { useState, useEffect } from 'react';
-import { PlusSquare, DashSquare } from 'react-bootstrap-icons';
 
 const LifeCounter: React.FC = () => {
   const [life, setLife] = useState<number>(20);
   const [totalChanges, setTotalChanges] = useState<number>(0);
 
   const handleIncrement = () => {
-    setLife(life + 1);
-    setTotalChanges(totalChanges + 1);
+    setLife(prevLife => prevLife + 1);
+    setTotalChanges(prevChanges => prevChanges + 1);
   };
 
   const handleDecrement = () => {
-    setLife(life - 1);
-    setTotalChanges(totalChanges - 1);
+    setLife(prevLife => prevLife - 1);
+    setTotalChanges(prevChanges => prevChanges - 1);
   };
 
   useEffect(() => {
-    const timeoutDuration: number = 1000;
-    const changeTimeout: NodeJS.Timeout = setTimeout(() => {
+    const timeoutDuration = 1000;
+    const changeTimeout = setTimeout(() => {
       setTotalChanges(0);
     }, timeoutDuration);
 
     return () => clearTimeout(changeTimeout);
   }, [totalChanges]);
 
+  const handleClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const rect = event.currentTarget.getBoundingClientRect();
+    const y = event.clientY - rect.top; // クリック位置を取得
+    if (y < rect.height / 2) {
+      handleIncrement(); // 上半分をクリックした場合、インクリメント
+    } else {
+      handleDecrement(); // 下半分をクリックした場合、デクリメント
+    }
+  };
+
   return (
     <div className="text-center mt-10">
-      <h1 className="text-3xl font-semibold">Life Counter</h1>
-      <h2 className="text-5xl font-bold my-5">{life}</h2>
-      <div className="flex justify-center space-x-4">
-        <button
-          className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-          onClick={handleIncrement}
-        >
-          <PlusSquare className="text-xl" /> +1
-        </button>
-        <button
-          className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
-          onClick={handleDecrement}
-        >
-          <DashSquare className="text-xl" /> -1
-        </button>
+      <div
+        style={{ cursor: 'pointer' }}
+        onClick={handleClick}
+      >
+        <h1 className="text-5xl font-bold my-5">{life}</h1>
       </div>
+      
       <div className="mt-8">
         <h3 className="text-xl font-semibold">合計変更</h3>
         <p className="text-2xl">{totalChanges}</p>
