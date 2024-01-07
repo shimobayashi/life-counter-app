f54677298fda86d76798c22d4dea6faa36fa5661
shimobayashi <akimasashimobayashi@gmail.com>
2024-01-07 18:54:24 +0900

    テキストを選択不能にした。

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import LifeCounter from '@/app/ui/life-counter';
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
+    <div className="flex flex-col items-center justify-center min-h-screen unselectable">
       <LifeCounter isInverted={true} />
       <LifeCounter />
     </div>
